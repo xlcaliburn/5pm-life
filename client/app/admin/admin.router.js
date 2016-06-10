1b852d5e146abf56357371715f15cfa8d2461660
xLCaliburn <michaelchunkitwong@gmail.com>
2016-06-10 00:34:14 -0400

    Begin adding admin code

--- client/app/admin/admin.router.js
+++ client/app/admin/admin.router.js
@@ -1,12 +1,23 @@
 'use strict';
 
 angular.module('fivepmApp.admin')
-  .config(function($stateProvider) {
-    $stateProvider.state('admin', {
-      url: '/admin',
-      templateUrl: 'app/admin/admin.html',
-      controller: 'AdminController',
-      controllerAs: 'admin',
-      authenticate: 'admin'
-    });
-  });
+	.config(function($stateProvider) {
+		$stateProvider
+			.state('admin', {
+				url: '/admin',
+				templateUrl: 'app/admin/admin.html',
+				// controller: 'AdminController',
+				// controllerAs: 'admin'
+				authenticate: 'admin'
+			})
+			.state('admin.tags', {
+				url: '/admin/tags',
+				templateUrl: 'app/admin/views/manage_tags.html',
+				authenticate: 'admin'
+			})
+			.state('admin.venues', {
+				url: '/admin/venues',
+				templateUrl: 'app/admin/views/manage_venues.html'
+			})
+		;
+	});
