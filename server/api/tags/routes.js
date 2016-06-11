1fa6123e7bac33870c0513e3a315c516a2dc9d85
xLCaliburn <michaelchunkitwong@gmail.com>
2016-06-10 21:22:49 -0400

    Add venue api calls and refactor routes

--- server/api/tags/routes.js
+++ server/api/tags/routes.js
@@ -1,13 +1,9 @@
 import {Router} from 'express';
 
 var mongoose = require('mongoose');
-var models = require('./model.js')(mongoose);
 
-var Venue = mongoose.model('Venue');
 var Enums = mongoose.model('Enums');
-
 var Tags 		= Enums.find({type : 'tag'});
-var Activity 	= Enums.find({type : 'activity'});
 
 var router = new Router();
 router.get('/', function(req,res) {
