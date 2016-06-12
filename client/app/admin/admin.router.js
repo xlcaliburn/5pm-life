1fb7e8d90bf8c29f79bc328c01e1f189f3a76fb3
xLCaliburn <michaelchunkitwong@gmail.com>
2016-06-11 20:16:58 -0400

    Add bootstrap-tags library for tags

--- client/app/admin/admin.router.js
+++ client/app/admin/admin.router.js
@@ -3,7 +3,7 @@
 angular.module('fivepmApp.admin')
 	.config(function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('/admin');
-		
+
 		$stateProvider
 			.state('admin', {
 				url: '/admin',
@@ -12,11 +12,14 @@ angular.module('fivepmApp.admin')
 				// controllerAs: 'admin'
 				// authenticate: 'admin'
 			})
+			.state('admin.users', {
+				url: '/users',
+				templateUrl: 'app/admin/views/manage_users.html'
+			})
 			.state('admin.tags', {
 				url: '/tags',
 				templateUrl: 'app/admin/views/manage_tags.html',
 				controller: "tagController"
-				// authenticate: 'admin'
 			})
 			.state('admin.venues', {
 				url: '/venues',
--- client/app/admin/controllers/venueController.js
+++ client/app/admin/controllers/venueController.js
@@ -2,6 +2,12 @@ angular.module('venueCtrl', [])
 	.controller('venueController', function($scope, $http, Venues) {
 		$scope.formData = {};
 
+        var newTags = $('#venue_tags').tags({
+            tagData:["boilerplate", "tags"],
+            suggestions:["basic", "suggestions"],
+            restrictTo:["basic"]
+        });
+
 		Venues.get()
 			.success(function(data) {
 				$scope.venues = data;
@@ -12,11 +18,12 @@ angular.module('venueCtrl', [])
 
 		$scope.createVenue = function() {
 			if (!$.isEmptyObject($scope.formData)) {
+				$scope.formData.tags = newTags.getTags();
 				Venues.create($scope.formData)
 					.success(function(data) {
 						$scope.formData = {};
 						$scope.venues = data;
-					});
+											});
 			}
 		};
 
