d40dbc589ece7e64a1b49312e5470aa451b9b9b6
lalaman <lalaman@live.ca>
2016-06-18 14:11:06 -0400

    Added settings template, controller, and less.

---
+++ client/app/main/settings/settings.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function() {
+    angular
+    .module('fivepmApp')
+    .controller('SettingsController', SettingsController);
+
+    /** @ngInject */
+    function SettingsController($http, $timeout, EmailService) {
+        var self = this;
+
+
+    }
+
+})();
