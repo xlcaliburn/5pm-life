476aa1cf0c6a8f3ea67d22edcb2d4c3c887fb5aa
Kelvin <kelvin@adhocstudio.ca>
2016-08-08 14:52:49 -0400

    Fixed custom materialize_select plugin.

--- client/assets/plugins/materialize-select/materialize.select.js
+++ client/assets/plugins/materialize-select/materialize.select.js
@@ -5,29 +5,20 @@ function materialize_select() {
 
     $("select").material_select();
 
-    $(".select-dropdown").on("open", function(event) {
-        if (!unique_id) return;
+    $(".select-dropdown").on("close", function() {
+        $(".select-dropdown.dropdown-content").fadeOut();
+    });
 
+    $(".select-dropdown[data-activates]").click(function() {
+        unique_id = $(this).attr("data-activates");
         var list = $("#" + unique_id);
+
         var parent = $(".select-dropdown[data-activates='" + unique_id + "']");
         list.css("width", parent.width() + "px");
         list.css("position", "absolute");
         list.css("top", "0px");
         list.css("left", "0px");
         list.fadeIn();
-    });
-
-    $(".select-dropdown").on("close", function(event) {
-        $(".select-dropdown.dropdown-content").fadeOut();
-    });
-
-    $(".select-dropdown[data-activates]").click(function() {
-        if ($(".select-dropdown.dropdown-content").is(":visible")) {
-            $(".select-dropdown").trigger("close");
-        } else {
-            unique_id = $(this).attr("data-activates");
-            $(".select-dropdown").trigger("open");
-        }
     });
 
 }
