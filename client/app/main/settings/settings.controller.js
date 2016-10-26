04afaad2f6d6062e1d03df5c2a0b7e3660cf04d9
Kelvin Chan <lalaman@live.ca>
2016-10-26 00:00:24 -0400

    Refactored settings controller

--- client/app/main/settings/settings.controller.js
+++ client/app/main/settings/settings.controller.js
@@ -3,38 +3,66 @@
         .module('fivepmApp')
         .controller('SettingsController', SettingsController);
 
-    function SettingsController($scope, user, Months, SettingsService) {
+    function SettingsController($scope, enumsData, Months, SettingsService, userData, Users) {
         var vm = this;
-        vm.user = user;
-        vm.format_birthday = function() { return moment(vm.user.birthday).utc().format('MMMM DD, YYYY'); };
-        vm.get_age = function() { return moment().diff(vm.user.birthday, 'years'); };
-        vm.select_image = function() { angular.element('input[name="userImage"]').click(); };
-        vm.upload = upload;
-        vm.profile_picture = 'default_profile.png';
 
-        function upload(target) {
+        // model
+        vm.user = userData;
+        vm.enums = enumsData;
+
+        // functions
+        vm.formatBirthday = formatBirthday;
+        vm.getAge = getAge;
+        vm.getEthnicity = getEthnicity;
+        vm.getGender = getGender;
+        vm.selectImage = selectImage;
+        vm.uploadImage = uploadImage;
+
+        // get user's age based on birthday
+        function getAge() {
+            return moment().diff(vm.user.birthday, 'years');
+        }
+
+        // get ethnicty value
+        function getEthnicity() {
+            return vm.enums.ethnicity[vm.user.ethnicity].value;
+        }
+
+        // get gender value
+        function getGender() {
+            return vm.enums.gender[vm.user.gender].value;
+        }
+
+        // format birthday to return age
+        function formatBirthday() {
+            return moment(vm.user.birthday).utc().format('MMMM DD, YYYY');
+        }
+
+        // open file picker
+        function selectImage() {
+            angular.element('input[name="userImage"]').click();
+        }
+
+        // upload new image
+        function uploadImage(target) {
             var target_input = angular.element(target)[0].files;
             var input_file = target_input[0];
             var formData = new FormData();
 
             formData.append('userImage', input_file);
             SettingsService.uploadProfilePicture(formData)
-                .then(function(res) {
-                    var response = res.data.response;
-                    if (response.status === 'ok') {
-                        Materialize.toast('Profile picture updated', 6000); // jshint ignore:line
-                        if (!vm.user.profile_picture) {
-                            vm.user.profile_picture = {
-                                current: response.file
-                            };
-                        } else {
-                            vm.user.profile_picture.current = response.file;
-                        }
+            .then(function(res) {
+                var response = res.data.response;
+                if (response.status === 'ok') {
+                    Users.getMe()
+                    .then(function(res) {
+                        vm.user = res.data;
 
                         // update user info for navbar
                         $scope.$emit('updateUser');
-                    }
-                });
+                    });
+                }
+            });
         }
     }
 })();
