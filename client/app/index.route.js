2ecbda55cd8273d1441ca0606579cfb6518ded20
lalaman <lalaman@live.ca>
2016-11-01 20:56:25 -0400

    Updated home content.

--- client/app/index.route.js
+++ client/app/index.route.js
@@ -51,13 +51,13 @@
 									{
 										title: 'BETA Test Launching on Nov 4/5!',
 										date: 'Oct 22, 2016',
-										content: '<img src="http://i.imgur.com/r4CVu8E.jpg" width="100%"> <p>We’re preparing to launching our BETA test in the next upcoming weeks. We have been working so hard for the past 6 months and thanks to your feedback and support, we’ve managed to bring you version 0.5! As we continue to get more feedback, I would like to make sure you’re not actually reading this but focusing on the overall layout of this page.</p>'
+										content: '<img src="assets/images/home-1.jpg" width="100%"> <p>We’re preparing to launching our BETA test in the next upcoming weeks. We have been working so hard for the past 6 months and thanks to your feedback and support, we’ve managed to bring you version 0.5! As we continue to get more feedback, I would like to make sure you’re not actually reading this but focusing on the overall layout of this page.</p>'
 									}
 								]
 							},
 							about: {
 								name: 'About',
-								content: '<div class="about-title">A Little Bit About 5PM</div><div class="about-content"><p>From the ground up, we’ve built this platform to help people meet others who also enjoy doing the same things they do. I am not good at writing copies so I will just write a bunch of text here to look like I know what I am talking about. Somebody please help me write a short copy about what 5PM.life is as well as why we chose to develop this platform to begin with.</p><p class="align-center"><img src="assets/images/5pm-icon-live.png" width="150px"><br /><span class="light purple-font">share your interests</span> &nbsp; · &nbsp;<span class="light pink-font">exchange ideas</span> &nbsp;·&nbsp; <span class="light aqua-font">create life experiences</span></p><p>From the ground up, we’ve built this platform to help people meet others who also enjoy doing the same things they do. I am not good at writing copies so I will just write a bunch of text here to look like I know what I am talking about. Somebody please help me write a short copy about what 5PM.life is as well as why we chose to develop this platform to begin with.</p>'
+								content: '<div class="about-title">A Little Bit About 5PM</div><div class="about-content"><p>Are you looking to meet new people but can\'t meet anyone outside of work? Want to try new events but can\'t get friends to go with you? Looking for a romantic partner but not clicking with anyone? 5PM is the newest platform that can connect you with the people that you want to meet.</p><p class="align-center"><img src="assets/images/5pm-icon-live.png" width="150px"><br /><span class="light purple-font">share your interests</span> &nbsp; · &nbsp;<span class="light pink-font">exchange ideas</span> &nbsp;·&nbsp; <span class="light aqua-font">create life experiences</span></p><p>5PM aims to matchmake you with the people that you want to actually want to meet while doing activities and events that you actually want to do. Our mission is to help provide a way for you to meet more people, make more friends and have fun!</p>'
 							},
 							feedback: {
 								name: 'Feedback',
