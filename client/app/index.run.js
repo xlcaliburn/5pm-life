d828f53506041df84ff7c201a68389f78a7ab844
lalaman <lalaman@live.ca>
2016-12-24 17:53:15 -0500

    Modified initial options and styling for PNotify.

--- client/app/index.run.js
+++ client/app/index.run.js
@@ -9,6 +9,19 @@
     /** @ngInject */
     function runBlock() {
 
+        // set PNotify theme
+        PNotify.prototype.options.styling = 'bootstrap3';
+        PNotify.prototype.options.buttons.sticker = false;
+        PNotify.prototype.options.buttons.closer_hover = false;
+        PNotify.prototype.options.delay = 6000;
+        PNotify.prototype.options.before_init = function(notice) {
+            setTimeout(function() {
+                var span = $(notice.stack.context[0].querySelectorAll('.alert .glyphicon.glyphicon-remove'));
+                span.html('close');
+                span.addClass('material-icons');
+                span.removeClass('glyphicon'); span.removeClass('glyphicon-remove');
+            });
+        };
     }
 
 })();
