eaa0508d3cee502d7bdaf975e28226f20af870a7
lalaman <lalaman@live.ca>
2016-12-26 03:17:33 -0500

    Not really sure what I did here. But I definitely did something.

--- client/app/main/shared/navbar/navbar.directive.js
+++ client/app/main/shared/navbar/navbar.directive.js
@@ -6,26 +6,13 @@
     .module('fivepmApp')
     .directive('navbar', NavbarDirective);
 
-    function NavbarDirective($rootScope, $state) {
+    function NavbarDirective() {
         return {
             restrict: 'E',
             templateUrl: 'app/main/shared/navbar/navbar.html',
             transclude : false,
             controller: 'NavbarController',
-            controllerAs: 'navbar',
-            link: function(scope) {
-                $rootScope.$on('$stateChangeSuccess', function() {
-                    if ($state.current.name === 'home.event') {
-                        scope.navbar.get_queue_status();
-                        scope.navbar.on_event_page = true;
-                    } else {
-                        scope.navbar.get_queue_status();
-                        scope.navbar.on_event_page = false;
-                    }
-                    scope.navbar.current_scope = $state.current.scopeTitle;
-                    return;
-                });
-            }
+            controllerAs: 'navbar'
         };
     }
 
