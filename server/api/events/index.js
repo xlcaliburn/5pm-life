c16fa300c89ef2bb0f0914c088c3823f5e0d3273
lalaman <lalaman@live.ca>
2016-12-25 22:39:15 -0500

    Re-ordered events api call so that it doesn't clash with other existing routes.

--- server/api/events/index.js
+++ server/api/events/index.js
@@ -7,12 +7,12 @@ var controller = require('./events.controller');
 var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
-router.post('/', auth.isAuthenticated(), controller.create);
 router.get('/all', auth.isAuthenticated(), controller.getAllEvents);
-
-router.get('/:id', auth.isAuthenticated(), controller.show);
-router.get('/admin/:id', auth.isAuthenticated(), controller.admin_show);
 router.get('/attendees/:event_id', auth.isAuthenticated(), controller.getAttendees);
+router.get('/admin/:id', auth.isAuthenticated(), controller.admin_show);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+
+router.post('/', auth.isAuthenticated(), controller.create);
 router.post('/confirm', auth.isAuthenticated(), controller.confirmEvent);
 router.post('/decline', auth.isAuthenticated(), controller.declineEvent);
 router.post('/leave', auth.isAuthenticated(), controller.leaveEvent);
