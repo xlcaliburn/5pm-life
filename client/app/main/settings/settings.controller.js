9d21886ab814c67baa6d3ea7d85c8461f357129c
Michael Wong <michaelchunkitwong@gmail.com>
2017-01-12 13:23:46 -0500

    Replace enum values in settings

--- client/app/main/settings/settings.controller.js
+++ client/app/main/settings/settings.controller.js
@@ -6,6 +6,8 @@
     function SettingsController($scope, enumsData, SettingsService, userData, Users, $uibModal) {
         var vm = this;
         vm.user = userData;
+        vm.ethnicity = null;
+        vm.gender = null;
         vm.enums = enumsData;
         vm.getAge = () => moment().diff(vm.user.birthday, 'years');
         vm.getEthnicity = () => typeof vm.enums.ethnicity[vm.user.ethnicity] === 'undefined' ? vm.user.ethnicity : vm.enums.ethnicity[vm.user.ethnicity].value;
@@ -20,6 +22,9 @@
         init();
 
         function init() {
+            vm.gender = enumsData.gender[vm.user.gender].value;
+            vm.ethnicity = enumsData.ethnicity[vm.user.ethnicity].value;
+
             $(document).ready(function(){
                 $('ul.tabs').tabs();
             });
