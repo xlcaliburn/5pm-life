be090cf0d88f5cc7ca09ceab75f325933977f192
Michael Wong <michaelchunkitwong@gmail.com>
2017-01-24 13:22:53 -0500

    One part left todo

--- client/app/admin/events/modals/eventModalController.js
+++ client/app/admin/events/modals/eventModalController.js
@@ -5,49 +5,24 @@
 
 	function EventModalController ($q, $timeout, $uibModalInstance, Events, Venues, Activities) {
 		var vm = this;
-		vm.selectChanged = selectChanged;
 		vm.submit = submit;
 		vm.selected_event = {};
-		vm.allowed_activities = {};
-		vm.allowed_venues = {};
-		vm.venues = {};
+		vm.allowed_activities = [];
+		vm.all_venues = [];
+		vm.allowed_venues = [];
 		vm.title = 'Create Event';
+		vm.filter_venues = filterVenues;
 
 		init();
-//WORKING HERE UNFINISHED, having only appropriate venues pop up for activity
-//		function init() {
-//			console.log("init start");
-//			Venues.get()
-//				.then(function(res) {
-//					vm.allowed_venues = res.data; //This is where the magic happens
-//					console.log(res);
-//					return Activities.get();
-//				})
-//				.then(function(res) {
-//					vm.allowed_activities = res.data;
-//					console.log(res);
-//					return res;
-//				})
-//				.then(function() {
-//					$timeout(function() {materialize_select();});
-//				})
-//				.catch(function(err) {
-//					console.log(err);
-//				});
-//		}
 
 		function init() {
-			console.log("init start");
 			Activities.get()
 				.then(function(res) {
-					vm.allowed_activities = res.data; //This is where the magic happens
-					console.log(res);
+					vm.allowed_activities = res.data;
 					return Venues.get();
 				})
 				.then(function(res) {
-					vm.allowed_venues = res.data;
-					vm.venues = vm.allowed_venues;
-					console.log(res);
+					vm.all_venues = res.data;
 					return res;
 				})
 				.then(function() {
@@ -57,17 +32,20 @@
 					console.log(err);
 				});
 		}
-//testCTRL for dropdown
-		function selectChanged($scope) {
-			console.log('function was called');
-			console.log(vm.selected_event.activity.tags);
-			console.log(Venues.get());
 
-			delete vm.allowed_venues[1];
-				delete vm.allowed_venues[2];
-					delete vm.allowed_venues[3];
+		function filterVenues()
+		{
+			var allowed = [];
 
-	}
+			/**
+
+				TODO
+
+			**/
+
+			vm.allowed_venues = allowed;
+			$timeout(function() {materialize_select();});
+		}
 
 		function submit() {
 			vm.selected_event.dt_search_start = new Date().getTime();
