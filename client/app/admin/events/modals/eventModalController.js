e80b0a61c9486029bc07cfae3ca1e2849a5f2e5d
KevvinG <kevingrzela@yahoo.com>
2017-01-24 11:45:36 -0500

    Work in progress

--- client/app/admin/events/modals/eventModalController.js
+++ client/app/admin/events/modals/eventModalController.js
@@ -5,22 +5,49 @@
 
 	function EventModalController ($q, $timeout, $uibModalInstance, Events, Venues, Activities) {
 		var vm = this;
+		vm.selectChanged = selectChanged;
 		vm.submit = submit;
 		vm.selected_event = {};
 		vm.allowed_activities = {};
 		vm.allowed_venues = {};
+		vm.venues = {};
 		vm.title = 'Create Event';
 
 		init();
+//WORKING HERE UNFINISHED, having only appropriate venues pop up for activity
+//		function init() {
+//			console.log("init start");
+//			Venues.get()
+//				.then(function(res) {
+//					vm.allowed_venues = res.data; //This is where the magic happens
+//					console.log(res);
+//					return Activities.get();
+//				})
+//				.then(function(res) {
+//					vm.allowed_activities = res.data;
+//					console.log(res);
+//					return res;
+//				})
+//				.then(function() {
+//					$timeout(function() {materialize_select();});
+//				})
+//				.catch(function(err) {
+//					console.log(err);
+//				});
+//		}
 
 		function init() {
-			Venues.get()
+			console.log("init start");
+			Activities.get()
 				.then(function(res) {
-					vm.allowed_venues = res.data;
-					return Activities.get();
+					vm.allowed_activities = res.data; //This is where the magic happens
+					console.log(res);
+					return Venues.get();
 				})
 				.then(function(res) {
-					vm.allowed_activities = res.data;
+					vm.allowed_venues = res.data;
+					vm.venues = vm.allowed_venues;
+					console.log(res);
 					return res;
 				})
 				.then(function() {
@@ -30,6 +57,17 @@
 					console.log(err);
 				});
 		}
+//testCTRL for dropdown
+		function selectChanged($scope) {
+			console.log('function was called');
+			console.log(vm.selected_event.activity.tags);
+			console.log(Venues.get());
+
+			delete vm.allowed_venues[1];
+				delete vm.allowed_venues[2];
+					delete vm.allowed_venues[3];
+
+	}
 
 		function submit() {
 			vm.selected_event.dt_search_start = new Date().getTime();
