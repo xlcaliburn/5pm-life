3cbfd2e62ff17ad1d2977ec80e7cf1e34b49a81b
KevvinG <kevingrzela@yahoo.com>
2017-01-25 21:56:26 -0500

    add filter to event modal finished
    All seems to be working with my at home testing

--- client/app/admin/events/modals/eventModalController.js
+++ client/app/admin/events/modals/eventModalController.js
@@ -5,16 +5,15 @@
 
 	function EventModalController ($q, $timeout, $uibModalInstance, Events, Venues, Activities) {
 		var vm = this;
+		vm.filterVenues = filterVenues;
 		vm.submit = submit;
 		vm.selected_event = {};
-		vm.allowed_activities = [];
-		vm.all_venues = [];
-		vm.allowed_venues = [];
+		vm.allowed_activities = {};
+		vm.allowed_venues = {};
+		vm.all_venues = {};
 		vm.title = 'Create Event';
-		vm.filter_venues = filterVenues;
 
 		init();
-
 		function init() {
 			Activities.get()
 				.then(function(res) {
@@ -22,7 +21,8 @@
 					return Venues.get();
 				})
 				.then(function(res) {
-					vm.all_venues = res.data;
+					vm.allowed_venues = res.data;
+					vm.all_venues = vm.allowed_venues;
 					return res;
 				})
 				.then(function() {
@@ -32,20 +32,23 @@
 					console.log(err);
 				});
 		}
-
-		function filterVenues()
-		{
-			var allowed = [];
-
-			/**
-
-				TODO
-
-			**/
-
-			vm.allowed_venues = allowed;
+		//This Fn ensures that only the appropriate venues are displayed when an
+		//activity is selected
+		function filterVenues($scope) {
+			vm.allowed_venues = [];
+			var venue_array = $.map(vm.all_venues, function(value, index) {
+				return [value];
+			});
+			var current_activity = vm.selected_event.activity.activity_name;
+			for (var k=0; k<venue_array.length; k++) {
+				for (var i=0; i<venue_array[k].allowed_activities.length; i++) {
+					if (current_activity == venue_array[k].allowed_activities[i].activity_name) {
+						vm.allowed_venues.push(venue_array[k]);
+					}//End if
+				};//End inside loop
+			};//End outside loop
 			$timeout(function() {materialize_select();});
-		}
+	}//End fn
 
 		function submit() {
 			vm.selected_event.dt_search_start = new Date().getTime();
